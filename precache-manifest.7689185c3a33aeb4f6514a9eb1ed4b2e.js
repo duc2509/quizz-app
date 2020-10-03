44b6e31b0dd4fe3a55871013d22a7d3d5dbe8967
duc2509 <brianng2599@gmail.com>
2020-10-03 18:22:24 +0700

    Updates

---
+++ precache-manifest.7689185c3a33aeb4f6514a9eb1ed4b2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4810222fc07bc65d8d2f19b0c03db417",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cad0b1414b7dd08cb4f",
+    "url": "/static/css/main.73da8537.chunk.css"
+  },
+  {
+    "revision": "22e51f8f12e6e57ce459",
+    "url": "/static/js/2.b689ee1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b689ee1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cad0b1414b7dd08cb4f",
+    "url": "/static/js/main.d7ba0671.chunk.js"
+  },
+  {
+    "revision": "c74a83eba2721b0e7360",
+    "url": "/static/js/runtime-main.cfd05356.js"
+  }
+]);
